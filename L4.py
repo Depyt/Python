a73768c0311551857a99f1e8bd134239ff0bd4aa
Depyt <73955929+Depyt@users.noreply.github.com>
2022-01-26 23:59:58 +0300

    Add files via upload

---
+++ L4.py
@@ -0,0 +1,41 @@
+#machin = ['bmw','mers','audi','subaru','lamborgini']
+#print(machin)
+#mes=f"Покажите новый авто {machin[1].title()}"
+#print(mes)
+#del_item= machin.pop(1)
+#print(del_item)
+#print(machin)
+#mes1=f"Покажите новый {machin[3].title()}"
+#print(mes1)
+#del_item1=machin.pop(3)
+#print(del_item1)
+#print(machin)
+
+#money=int(input('Ваш бюджет '))
+#years=int(input('через сколько снимите '))
+#proc=int(input('Какой процент годовых'))
+
+#if years>11:
+#   money+=money*0.2
+#   print(money)
+#else:
+#    print(money)
+   
+
+#soum=5000
+#percent=20
+#year=int(input('Укажите месяц снятия: '))
+#if (year>11) and (year<24):
+#    soum_one_year=soum+soum*(percent/100)
+#    print('Ваша сумма равна: ', soum_one_year)
+#elif year>23:
+##    soum_one_year_1=soum+soum*(percent/100)
+#    soum_two_year=soum_one_year_1+soum_one_year_1*(percent/100)
+#    print('Ваша сумма равна: ', soum_two_year)
+#else:
+#    print('Ваша сумма равна: ', soum)
+
+
+
+
+
